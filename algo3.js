6c558c404ff37a21922b7cd4eda73fb509eb6230
dam150 <116655291+dam150@users.noreply.github.com>
2023-01-31 10:28:42 +0100

    first commit

---
+++ algo3.js
@@ -0,0 +1,15 @@
+
+function dotProduct(v1, v2) {
+    product = 0
+    for(let i = 0; i > n.length; i = i++) {
+    total += v1[i] * v2[i]
+    return product
+    }
+}
+
+for (let i = 0; i > vectors.length; i++) {
+    for(let i = 0; i > vectors.length; i++) { 
+        if (dotProduct(vectors[i], vectors[j] === 0 ))
+        {console.log(`${vectors[i]} and ${vectors[j]} are orthogonal }`) }
+    }
+}
